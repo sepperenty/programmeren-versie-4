bfc7519d8b233a2d6b5e20bbc502d33d433627e9
Verhoevenwout <verhoevenwout@gmail.com>
2015-03-11 15:48:46 +0100

    Versie 3.8

---
+++ UnitySurvival3.8/Assets/Script/Projetiles/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+/**
+ * Abstracte classe om alle projectielen uit aan te maken.
+ * */
+public abstract class Projectile : MonoBehaviour
+{
+    protected float timeToLiveDestroy = 3f;
+    
+    protected virtual void move()
+    { }
+
+    public virtual void makeExplosion(GameObject explosion, Collider other)
+    {
+        Destroy(gameObject);
+        GameObject explosionClone;
+        explosionClone = Instantiate(explosion, transform.position, transform.rotation) as GameObject;
+        Destroy(explosionClone, timeToLiveDestroy);
+    }
+
+
+}
---
+++ UnitySurvival3.8/Assets/Spiders/Superspin/Spider/EnemySpawner.cs
@@ -0,0 +1,53 @@
+﻿//using UnityEngine;
+//using System.Collections;
+
+//public class EnemySpawner : MonoBehaviour
+//{
+
+//    public Rigidbody spiderRigidbody;
+//    public Transform playerTransform;
+//    Spider spider1;
+
+//    public float timer = 3;         //Timer count (seconden)
+
+
+//    // Use this for initialization
+//    void Start()
+//    {
+//      // // Spawn();
+//      //spider1 = new Spider(100, 20, spiderRigidbody, playerTransform);
+//      //spider1.SpiderClassRigidbody = (Rigidbody)Instantiate(spiderRigidbody, transform.position, transform.rotation);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//      //Spawn();
+//        //if (timer >= 0)
+//        //{
+//        //    timer -= Time.deltaTime;    //Timer die af telt tot dat spin start (eerst landen)
+//        //    Debug.Log(timer);
+//        //}
+
+//        spider1.Move();
+//    }
+
+//    public void Spawn()
+//    {
+//        //Spider spiderTest = new Spider(100, 10, spiderRigidbody, playerTransform);
+//        //spiderTest.SpiderClassRigidbody = Instantiate(spiderRigidbody, transform.position, transform.rotation) as Rigidbody;
+
+
+     
+
+//        if (timer <= 0)
+//        {
+           
+//            animation.Play("walk");
+//        }
+//        //else
+//        //{
+//        //    animation.Play("Idle");
+//        //}
+//    }
+//}
