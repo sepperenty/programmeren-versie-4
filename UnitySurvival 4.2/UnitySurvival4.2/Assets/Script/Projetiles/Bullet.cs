ee47729510a48ad02798c157e9602b833b73c238
Verhoevenwout <verhoevenwout@gmail.com>
2015-03-13 21:29:21 +0100

    Versie 4.2
    -Nieuwe map met het klassediagram in + screenshot
    -Er is nu duidelijk verschil tussen verschillende spinnen. Spider is
    groen, superspider is blauw/wit en boss is rood
    -Ook heb ik een toggle toegevoegd zodat je met ESC het pauzemenu kan aan
    en uit zetten.
    
    Nog te doen:
    -(blauwe) explosie bij teleport??
    -Laten zien wanneer player geraakt wordt??
    -Aantal kogels??

---
+++ UnitySurvival 4.2/UnitySurvival4.2/Assets/Script/Projetiles/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : Projectile
+{
+  public float shootDistance = 500f;
+  private float damage = 0.1f;
+  private const string HIT_BY_BULLET = "HitByBullet";
+
+  //De methode move van de SuperKlasse wordt overschreven.
+  // Er wordt een rayCast gezet in de richting van waar er gemikt wordt.
+  // ALs deze ray een spin raakt wordt er een methode in deze spin opgeroepen: hitbybullet.De damage wordt hier ook aan meegegeven.
+  protected override void move()
+  {
+    RaycastHit hit;
+    Ray shootingRay = new Ray(transform.position, transform.forward * shootDistance);
+    Debug.DrawRay(transform.position, transform.forward * shootDistance);
+    //GameObject shootFireclone = Instantiate(shootFire, transform.position, transform.rotation) as GameObject;
+
+    //Destroy(shootFireclone, 0.5f);
+
+    if (Physics.Raycast(shootingRay, out hit))
+    {
+      if (hit.collider.tag == DestroyedCityConstants.SPIDER_TAG)
+
+      {
+        hit.collider.SendMessage(DestroyedCityConstants.HIT_BY_BULLET, damage);
+        Debug.Log("hit");
+      }
+    }
+  }
+}
